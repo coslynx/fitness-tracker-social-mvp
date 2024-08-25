a269ecbe9392a4f6f4f232b981440e09c1af4275
CosLynx AI <drixisgod@gmail.com>
2024-08-25 13:58:14 +0530

    generated file: `tailwind.config.ts`

---
+++ tailwind.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+};
